f4a57f46008b4d39bd89e5c24e46b7b9cb2a2d3a
SAHNOUNGhaith-07 <ghaith.sahnoun@esprit.tn>
2022-02-27 21:36:52 +0100

    Update GildedRoseTest.java

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-
+/*
 	@Test
 	void foo() {
 		Item[] items = new Item[] { new Item("foo", 0, 0) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
 		assertEquals("fixme", app.items[0].name);
-	}
+	}*/
 
 	@Test
 	public void testUpdateNormaltem() throws Exception {
