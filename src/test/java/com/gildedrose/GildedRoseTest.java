6c74fa2ece8ca7b72031ec6a3ce605feeed46a64
SAHNOUNGhaith-07 <ghaith.sahnoun@esprit.tn>
2022-02-27 21:35:10 +0100

    Update GildedRoseTest.java

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,68 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	void foo() {
+		Item[] items = new Item[] { new Item("foo", 0, 0) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals("fixme", app.items[0].name);
+	}
+
+	@Test
+	public void testUpdateNormaltem() throws Exception {
+
+		Item item = new Item("+5 Dexterity Vest", 10, 20);
+		Item item2 = new Item("Aged Brie", 2, 0);
+		Item[] items = new Item[] { item, item2 };
+		GildedRose app = new GildedRose(items);
+
+		app.updateQuality();
+
+		assertEquals(9, app.items[0].sellIn);
+		assertEquals(18, app.items[0].quality);
+	}
+
+	@Test
+	public void testUpdateAgedBackstageItem() throws Exception {
+
+		Item item = new Item("+5 Dexterity Vest", 10, 20);
+		Item item2 = new Item("Aged Brie", 2, 0);
+		Item[] items = new Item[] { item, item2 };
+		GildedRose app = new GildedRose(items);
+
+		app.updateQuality();
+
+		assertEquals(1, app.items[1].sellIn);
+		assertEquals(3, app.items[1].quality);
+	}
+
+	@Test
+	public void testUpdateSulfurasItem() throws Exception {
+
+		Item item = new Item("Sulfuras", 10, 20);
+		Item item2 = new Item("Aged Brie", 2, 0);
+		Item[] items = new Item[] { item, item2 };
+		GildedRose app = new GildedRose(items);
+
+		app.updateQuality();
+
+		assertEquals(10, app.items[0].sellIn);
+		assertEquals(20, app.items[0].quality);
+	}
+
+	@Test
+	public void testUpdateConjuredItem() throws Exception {
+
+		Item item = new Item("Conjured", 10, 20);
+		Item item2 = new Item("Aged Brie", 2, 0);
+		Item[] items = new Item[] { item, item2 };
+		GildedRose app = new GildedRose(items);
+
+		app.updateQuality();
+
+		assertEquals(9, app.items[0].sellIn);
+		assertEquals(16, app.items[0].quality);
+	}
 
 }
